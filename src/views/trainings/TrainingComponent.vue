83df8692234d53ee0957009d9dd3eb166519a8e6
vicsystems <victorasuquob@gmail.com>
2023-12-15 13:14:30 +0100

    update on training modules

--- src/views/trainings/TrainingComponent.vue
+++ src/views/trainings/TrainingComponent.vue
@@ -3,33 +3,116 @@
     <div class="card table-responsive">
       <div class="card-body">
 
-      <div class="d-flex justify-content-start flex-wrap">
-        <div style="min-width: 200px;"  v-for="trainingProgramme,index in trainingProgrammes" :key="trainingProgramme.id" class="border m-1">
-          <h6>{{ index + 1 }}. {{ trainingProgramme.name }}</h6>
+        <b-button
+          :id="'modalBtn'"
+          v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+          v-b-modal.modal-no-footer
+          v-b-modal.modal-1
+
+          class="d-none"
+          variant="outline-primary"
+        >
+          Basic Modal
+        </b-button>
+
+        <b-modal
+          id="modal-1"
+          :footer-class="'d-none'"
+          :title="'Update Training for : '+selectedName"
+        >
+          <b-card-text>
+
+            <div class="form-group">
+              <select
+                id=""
+                v-model="selStatus"
+                class="form-control"
+              >
+                <option :value="'0'">
+                  Not Trained
+                </option>
+                <option :value="'1'">
+                  Familiar
+                </option>
+                <option :value="'2'">
+                  Trained
+                </option>
+                <option :value="'3'">
+                  Expert
+                </option>
+
+              </select>
+            </div>
+            <div class="form-group">
+              <button
+                class="btn btn-primary btn-"
+                @click="postUpdate()"
+              >
+                Update
+              </button>
+            </div>
+          </b-card-text>
+        </b-modal>
+
+        <div class="d-flex justify-content-start flex-wrap">
+          <div
+            v-for="trainingProgramme,index in trainingProgrammes"
+            :key="trainingProgramme.id"
+            style="min-width: 200px;"
+            class="border m-1"
+          >
+            <h6>{{ index + 1 }}. {{ trainingProgramme.name }}</h6>
+          </div>
+
         </div>
 
-      </div>
+        <h6>0. Not Trained</h6>
+        <h6>1. Familiar</h6>
+        <h6>2. Trained</h6>
+        <h6>3. Expert</h6>
 
         <table class="table">
           <tr>
-            <td></td>
-            <th v-for="trainingProgramme,index in trainingProgrammes" :key="trainingProgramme.id">
+            <td />
+            <th
+              v-for="trainingProgramme,index in trainingProgrammes"
+              :key="trainingProgramme.id"
+            >
               {{ index+1 }}
 
             </th>
           </tr>
-          <tr v-for="staffRecord in staffRecords" :key="staffRecord.id">
+          <tr
+            v-for="staffRecord in staffRecords"
+            :key="staffRecord.id"
+          >
             <td>{{ staffRecord.fullname }}
-            <br>
-            <button @click="generate(staffRecord.id)" class="btn btn-primary btn-sm">refresh</button>
+              <br>
+              <button
+                class="btn btn-primary btn-sm"
+                @click="generate(staffRecord.id)"
+              >
+                refresh
+              </button>
             </td>
-         
-            <td v-for="training in staffRecord.staff_trainings" :key="training.id" class="border">
+
+            <td
+              v-for="training in staffRecord.staff_trainings"
+              :key="training.id"
+              class="border"
+            >
 
               {{ training.grade }}
-              
+
+              <feather-icon
+                class="text-success"
+                icon="EditIcon"
+                size="10"
+                @click="updateStatus(training.id, staffRecord.fullname)"
+              />
+
             </td>
-           
+
           </tr>
         </table>
 
@@ -39,13 +122,25 @@
 </template>
 <script>
 import axios from 'axios'
+import {
+  BModal, BButton, BCardText,
+} from 'bootstrap-vue'
 
 export default {
+  components: {
+
+    BButton,
+    BModal,
+    BCardText,
+  },
   data() {
     return {
       trainingProgrammes: [],
       staffRecords: [],
       staffTrainings: [],
+      selectedName: '',
+      selectedTrainingId: '',
+      selStatus: '',
     }
   },
   mounted() {
@@ -54,6 +149,30 @@ export default {
     // this.getStaffTrainings()
   },
   methods: {
+
+    postUpdate() {
+      axios({
+        url: `${process.env.VUE_APP_BACKEND_URL}/api/staff-trainings`,
+        method: 'put',
+        data:{
+          grade: this.selStatus
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }).then(res => {
+        console.log(res.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    updateStatus(selectedTrainingId, selectedName) {
+      document.getElementById('modalBtn').click()
+
+      this.selectedName = selectedName
+      this.selectedTrainingId = selectedTrainingId
+    },
     getTrainings() {
       axios({
         url: `${process.env.VUE_APP_BACKEND_URL}/api/training-programmes`,
@@ -104,7 +223,6 @@ export default {
     },
 
     generate(staffRecordId) {
-
       alert(staffRecordId)
 
       axios({
@@ -119,7 +237,7 @@ export default {
         },
       }).then(res => {
         console.log(res)
-   
+
         this.getStaffRecords()
       }).catch(error => {
         console.log(error)
